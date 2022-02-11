1c6ec2b6f5931b23c71ba3e74ed978d85e98cff4
wesleylcj <wesleylcj@gmail.com>
2022-02-11 13:20:59 -0300

    variaveis ate if

--- src/aula01/Principal.java
+++ src/aula01/Principal.java
@@ -5,12 +5,19 @@ import util.Util;
 
 public class Principal {
     public static void main(String[] args) {
+
+
+
         System.out.println("informe seus rendimentos:");
         float receita= Util.lerNumeroComVirgulaUsuario();
         System.out.println(" informe suas despesas:");
         float despesas= Util.lerNumeroComVirgulaUsuario();
         float saldo= receita-despesas;
         System.out.println(" Saldo:R$"+saldo);
+
+
+
+
 
 
 
---
+++ src/aula2/Aula2.java
@@ -0,0 +1,60 @@
+package aula2;
+
+import util.Util;
+
+public class Aula2 {
+
+    public static void main(String[] args) {
+
+        codigoAbastecimento();
+        codigoIdadae();
+        codigoMesaeCadeira();
+
+    }
+
+    private static void codigoAbastecimento() {
+        System.out.println("inicio de programa calculo de combustivel");
+        System.out.println("informe o valor do abastecimento:");
+
+        double abastecimento = Util.letNumeroInteiroUsuario();
+
+        double gasolina = 7;
+
+        double litros = abastecimento / gasolina;
+
+        System.out.println("total:" + litros);
+        System.out.println("fim do calculo");
+    }
+
+
+    private static void codigoIdadae() {
+        System.out.println("Informe sua idade:");
+        int idadeUsuario = Util.letNumeroInteiroUsuario();
+        if (idadeUsuario <= 16) {
+            System.out.println("acesso negado");
+        } else {
+            //System.out.println("acesso permitido");
+        }
+
+
+    }
+
+    private static void codigoMesaeCadeira() {
+        System.out.println("informe o numero de cadeiras:");
+        int numeroCadeiras = Util.letNumeroInteiroUsuario();
+
+        //se numero de cadeira é igual a 4
+        if (numeroCadeiras == 4) {
+            //numero de cadeira é 4, então imprime o preco 2000
+            System.out.println("preço é R$2000,00");
+        } else if (numeroCadeiras == 6) {
+            //número de cadeiras é 6 cadeiras então imprime o preço de 2400
+            System.out.println("preço é R$2400,00");
+        } else {
+            //senão a opção é inválida
+            System.out.println("Opção inválida, só temos mesa com 4 ou 6 cadeiras");
+        }
+    }
+
+
+}
