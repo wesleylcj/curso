b3a167b925a31c266a5db50324f93a156e844b85
wiliamjcj <wiljcj@gmail.com>
2022-02-11 18:43:12 -0300

    aadicionando exercicios

--- src/aula3OperadoresCondicional/Exercicios.java
+++ src/aula3OperadoresCondicional/Exercicios.java
@@ -1,7 +1,58 @@
 package aula3OperadoresCondicional;
 
+import util.Util;
+
 public class Exercicios {
+
     public static void main(String[] args) {
+        //exercicio 1
+        calcularIdadeEmDias();
+
+        //exercicio 2
+        //calcularMedidasCircunferencia();
+
+        //exercicio 3
+        //doarSangue();
+
+        //exercicio 4
+        //decidirEntreAlcoolGasolina();
+    }
+
+    /*
+        1 - Faça um algoritmo que leia a idade de uma pessoa expressa em anos, meses e dias e escreva a idade
+        dessa pessoa expressa apenas em dias. Considerar ano com 365 dias e mês com 30 dias.
+    */
+    static void calcularIdadeEmDias() {
+
+    }
+
+    /*
+        2 -   Escreva um programa que pergunte o raio de uma circunferência,
+        e sem seguida mostre o diâmetro, perimetro e área da circunferência
+        formulas:
+        diametro é igual duas vezes o raio
+        perimetro é igual ao diâmetro vezes o número PI
+        area é igual ao número PI vezes o raio ao quadrado (PI * raio * raio)
+    */
+    static void calcularMedidasCircunferencia() {
+        double numeroPi = Math.PI;
+
+    }
+
+    /*
+        3 - Para doar sangue é necessário ter entre 18 e 67 anos.
+        Faça um programa que pergunte a idade de uma pessoa e diga se ela pode doar sangue ou não.
+    */
+    static void doarSangue() {
+
+    }
+
+    /*
+        4 - O álcool tem uma eficiência 30% menor do que a gasolina, baseado nisso faça um programa que leia os preços do litro
+        do álcool e da gasolina e indique ao usuário qual melhor combustível para abastecer o carro.
+        dica: se o preco do alcool for maior ou igual a 70% do preço da gasolina não vale a pena.
+    */
+    static void decidirEntreAlcoolGasolina() {
 
     }
 }
