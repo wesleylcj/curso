74a431ae564217d5e76e4ef41ad3065a3c38c8f8
wiliamjcj <wiljcj@gmail.com>
2022-02-21 18:38:44 -0300

    adicionando exercicios

---
+++ src/exercicios/Exercicios1.java
@@ -0,0 +1,94 @@
+package exercicios;
+
+public class Exercicios1 {
+
+    public static void main(String[] args) {
+
+    }
+
+     /*
+    Faça um programa que faça 5 perguntas para uma pessoa sobre um crime.
+    As perguntas são:
+        “Telefonou para a vítima? “
+        “Esteve no local do crime?”
+        “Mora perto da vítima? “
+        “Devia para a vítima? “
+        “Já trabalhou com a vítima? “
+
+    O programa deve no final emitir uma classificação sobre a participação da pessoa no crime.
+    Se a pessoa responder positivamente a 2 questões ela deve ser classificada como “Suspeita”,
+    entre 3 e 4 como “Cúmplice”
+    e 5 como “Assassino“.
+    Caso contrário, ele será classificado como “Inocente“.
+    */
+    static void investigarCrime(){
+
+    }
+
+    // O usuário irá informar dois números e o programa irá imprimir o maior destes dois números
+    static void maiorNumero(){
+
+    }
+
+    /*
+     *Faça um programa que receba um número inteiro e informe se é positivo ou negativo e se é par ou impar.
+     * use operador resto % para verificar par ou impar
+     */
+    static void posistivoNegativoParImpar(){
+
+    }
+
+    /*
+    Faça um Programa que leia um número e exiba o dia correspondente da semana. (1- Domingo , 2- Segunda, etc.)
+    se digitar outro valor deve aparecer “valor inválido)
+    */
+    static void diaDaSemana(){
+
+    }
+
+    /*
+     Faça um programa para calcular a média ponderada de um aluno.
+     A prova 1 terá peso 1 a prova 2 peso, P3 peso 3 e P4 peso 4.
+     O aluno deverá informar as notas das 4 provas e o programa deverá mostrar qual é a média e se está aprovado ou reprovado.
+     A média de aprovação é 6.
+     */
+    static void calcularMedia(){
+
+    }
+
+    /*
+     - Num evento os participantes receberão um crachá com um número x por ordem de chegada no seguinte formato "[x]".
+    Usando while imprima todos os crachás necessários para a quantidade de participantes a ser informada pelo usuário.
+    EX: para 3 participantes, imprimir
+    [1]
+    [2]
+    [3]
+     */
+    static void imprimirCracha() {
+
+    }
+
+    /*
+    4 - Faça um programa que leia um número inteiro do usuário e usando while imprima a sua tabuada.
+    */
+    static void calcularTabuada(){
+
+    }
+
+    /* DIFICIL
+     5 - Usando while faça um programa que leia a renda de todos os integrantes de uma familia,
+      calcule e imprima a renda familiar per capita.
+    */
+    static void calcularRendaPerCapita(){
+
+    }
+
+     /*DIFICIL
+    Faça um programa que calcule quantos metros quadrados tem uma casa e imprima a sua área.
+    O usuário deverá informar o nome, a largura e o comprimento de cada cômodo, podendo informar N número de cômodos.
+    O programa deverá então calcular a área de cada cômodo, imprimir as áreas calculadas de cada cômodo e a área total.
+    */
+    static void calculoArea(){
+
+    }
+}
