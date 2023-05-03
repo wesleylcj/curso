d592b10d1facb83cd1374018890f13dbe544e483
wcarvalh <william.carvalho@avid.com>
2023-05-03 11:18:19 -0300

    alterando descrição do exercicio

--- src/cadastroUsuario/Main.java
+++ src/cadastroUsuario/Main.java
@@ -3,10 +3,12 @@ package cadastroUsuario;
 public class Main {
     public static void main(String[] args) {
         /* Crie uma estrutura usando while para um programa de cadastro de clientes.
-        * O programa deverá perguntar se o usuário deseja cadastrar um novo cliente ou se deseja sair,
-        * se sim o programa deverá perguntar as seguintes informações: Nome do cliente, cpf e idade. Em seguida deverá printar
-        * Cliente <nome> com CPF: <cpf> e idade <idade> anos, foi cadastrado com sucesso. Em seguida deve voltar ao inicio
-        * e perguntar novamente se deseja cadastrar um novo cliente ou sair. Se o usuário escolher sair o programa deverá ser encerrado.*/
+        * O programa deverá perguntar se o usuário deseja cadastrar um novo cliente ou se deseja sair.
+        * - Se o usuário escolher cadastrar cliente, o programa deverá perguntar as seguintes informações:
+        * Nome do cliente, cpf e idade, em seguida deverá printar
+        * "Cliente <nome> com CPF: <cpf> e idade <idade> anos, foi cadastrado com sucesso."
+        * e em seguida deve voltar ao inicio e perguntar novamente ao usuário qual ação deseja fazer.
+        * - Se o usuário escolher sair o programa deverá ser encerrado mostrando uma mensagem de encerramento. */
 
 
     }
